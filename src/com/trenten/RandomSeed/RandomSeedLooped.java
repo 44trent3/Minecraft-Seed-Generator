9623caec0d42b4415428cee450e33aa715a35143
44trent3 <trenten102579@gmail.com>
2020-05-16 19:15:44 -0500

    Fix indentation

--- src/com/trenten/RandomSeed/RandomSeedLooped.java
+++ src/com/trenten/RandomSeed/RandomSeedLooped.java
@@ -5,8 +5,8 @@ public class RandomSeedLooped {
   public static void main(String[] args) {
     Random rand = new Random();
     for(int d = 0; d != 5; ++d) {
-    System.out.printf("%12d ",rand.nextLong());
-    System.out.println();
+       System.out.printf("%12d ",rand.nextLong());
+       System.out.println();
     }
   }
 }
